25e57a4efb668cf97e961f6a214d4f2520ec48e5
fenixzava <fenixzava@gmail.com>
2022-11-29 22:24:36 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет
+// все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int m = Prompt("Введите число M: ");
+int n = Prompt("Введите число N: ");
+
+// нахождение четных чисел от M до N
+string FindingEvenNumbers(int m,  int n)
+{
+    string value = String.Empty;
+    for(int i = m; i<=n; i++)
+    if (m % 2==0)
+    {
+    m = m+1;
+    value = m + FindingEvenNumbers(m, n); 
+    }
+    
+    return value;
+}
+
+// вызов функции "четных чисел"
+
+string NaturalNumbers( int m,  int n)
+{
+    if(m<=n) return m + FindingEvenNumbers(m , n-1);
+    else return String.Empty;
+    
+}
+System.Console.WriteLine();
+System.Console.WriteLine(NaturalNumbers(  m,   n));
