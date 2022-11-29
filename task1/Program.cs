ba49a8b60481f78a009497225a897703b28ec85f
fenixzava <fenixzava@gmail.com>
2022-11-29 23:50:19 +0300

    task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -12,34 +12,29 @@ int Prompt(string message)
 
 int m = Prompt("Введите число M: ");
 int n = Prompt("Введите число N: ");
-
+// числа натуральные
+if (n < 1 || m < 1)
+{
+    Console.WriteLine("N и M не натуральные числа");
+    return;
+}
+// когда N < M
+if (n < m)
+{
+    Console.WriteLine("N меньше M");
+    return;
+}
 // нахождение четных чисел от M до N
-string FindingEvenNumbers(int m,  int n)
+
+PrintEvenNumbers(m, n);
+
+void PrintEvenNumbers(int m, int n)
 {
-    string value = String.Empty;
-    for(int i = m; i<=n; i++)
+    if (m % 2 == 0)
     {
-    if (m % 2==0)
-    {
-    return value;
+        Console.Write(n - m > 1 ? $"{m}, " : $"{m}");
     }
-    else
-    {
-    m = m+1;
-    value = m + FindingEvenNumbers(m, n);
-    }
-    }
-    
-    return value;
+    if(m == n) return;  
+    PrintEvenNumbers(m + 1, n);
 }
 
-// вызов функции "четных чисел"
-
-string NaturalNumbers( int m,  int n)
-{
-    if(m<=n) return m + FindingEvenNumbers(m+1 , n);
-    else return String.Empty;
-    
-}
-System.Console.WriteLine();
-System.Console.WriteLine(NaturalNumbers(  m,   n));
